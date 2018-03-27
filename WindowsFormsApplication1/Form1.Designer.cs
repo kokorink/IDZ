3a6fa9992f5b1165289de13592b918a3508fae63
kvk7 <kvk7@B10-109-ST09>
2018-03-27 18:31:43 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxRadius = new System.Windows.Forms.TextBox();
+            this.textBoxAngle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadJSONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBoxFile = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxRadius
+            // 
+            this.textBoxRadius.Location = new System.Drawing.Point(113, 29);
+            this.textBoxRadius.Name = "textBoxRadius";
+            this.textBoxRadius.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRadius.TabIndex = 1;
+            // 
+            // textBoxAngle
+            // 
+            this.textBoxAngle.Location = new System.Drawing.Point(112, 55);
+            this.textBoxAngle.Name = "textBoxAngle";
+            this.textBoxAngle.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAngle.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Angle";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBoxResult.Location = new System.Drawing.Point(113, 110);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResult.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemMenu,
+            this.loadMenuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(461, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItemMenu
+            // 
+            this.toolStripMenuItemMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemLoad,
+            this.toolStripMenuItemSave});
+            this.toolStripMenuItemMenu.Name = "toolStripMenuItemMenu";
+            this.toolStripMenuItemMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.S)));
+            this.toolStripMenuItemMenu.Size = new System.Drawing.Size(77, 20);
+            this.toolStripMenuItemMenu.Text = "Save Menu";
+            // 
+            // toolStripMenuItemLoad
+            // 
+            this.toolStripMenuItemLoad.Name = "toolStripMenuItemLoad";
+            this.toolStripMenuItemLoad.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.J)));
+            this.toolStripMenuItemLoad.Size = new System.Drawing.Size(167, 22);
+            this.toolStripMenuItemLoad.Text = "Save JSON";
+            this.toolStripMenuItemLoad.Click += new System.EventHandler(this.toolStripMenuItemSaveJSON_Click);
+            // 
+            // toolStripMenuItemSave
+            // 
+            this.toolStripMenuItemSave.Name = "toolStripMenuItemSave";
+            this.toolStripMenuItemSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.toolStripMenuItemSave.Size = new System.Drawing.Size(167, 22);
+            this.toolStripMenuItemSave.Text = "Save XML";
+            this.toolStripMenuItemSave.Click += new System.EventHandler(this.toolStripMenuItemSaveXML_Click);
+            // 
+            // loadMenuToolStripMenuItem
+            // 
+            this.loadMenuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadJSONToolStripMenuItem,
+            this.loadXMLToolStripMenuItem});
+            this.loadMenuToolStripMenuItem.Name = "loadMenuToolStripMenuItem";
+            this.loadMenuToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.L)));
+            this.loadMenuToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.loadMenuToolStripMenuItem.Text = "Load Menu";
+            // 
+            // loadJSONToolStripMenuItem
+            // 
+            this.loadJSONToolStripMenuItem.Name = "loadJSONToolStripMenuItem";
+            this.loadJSONToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.J)));
+            this.loadJSONToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.loadJSONToolStripMenuItem.Text = "Load JSON";
+            this.loadJSONToolStripMenuItem.Click += new System.EventHandler(this.loadJSONToolStripMenuItem_Click);
+            // 
+            // loadXMLToolStripMenuItem
+            // 
+            this.loadXMLToolStripMenuItem.Name = "loadXMLToolStripMenuItem";
+            this.loadXMLToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.loadXMLToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.loadXMLToolStripMenuItem.Text = "Load XML";
+            this.loadXMLToolStripMenuItem.Click += new System.EventHandler(this.loadXMLToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "File";
+            // 
+            // richTextBoxFile
+            // 
+            this.richTextBoxFile.Location = new System.Drawing.Point(34, 148);
+            this.richTextBoxFile.Name = "richTextBoxFile";
+            this.richTextBoxFile.ReadOnly = true;
+            this.richTextBoxFile.Size = new System.Drawing.Size(419, 98);
+            this.richTextBoxFile.TabIndex = 5;
+            this.richTextBoxFile.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 256);
+            this.Controls.Add(this.richTextBoxFile);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxAngle);
+            this.Controls.Add(this.textBoxRadius);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Площадь круга с исключённым сектором";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxRadius;
+        private System.Windows.Forms.TextBox textBoxAngle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemLoad;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem loadMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadJSONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadXMLToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBoxFile;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+
+
+namespace WindowsFormsApplication1
+{
+  
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+        [Serializable]
+
+        public struct RingSquare
+        {
+            public double r;
+            public double a;
+            public double s;
+        }
+
+        RingSquare ring;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            ring.r = Convert.ToDouble(textBoxRadius.Text);
+            ring.a = Convert.ToDouble(textBoxAngle.Text);
+            ring.s = Math.PI * Math.Pow(ring.r, 2) * (1 - ring.a / 360);
+            textBoxResult.Text = Convert.ToString(ring.s);
+            richTextBoxFile.Clear();
+        }
+        
+        private void toolStripMenuItemSaveJSON_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = "c:\\";
+            saveFileDialog.Filter = "JSON files (*.json)|*.json";
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+
+                    DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(RingSquare));
+                    jsonSerializer.WriteObject(myStream, ring);
+                    myStream.Close();
+                }
+            }
+        }
+        
+        private void toolStripMenuItemSaveXML_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = "c:\\";
+            saveFileDialog.Filter = "XML files (*.xml)|*.xml";
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+                    XmlSerializer xmlFormatter = new XmlSerializer(typeof(RingSquare));
+                    xmlFormatter.Serialize(myStream, ring);
+                    myStream.Close();
+                }
+            }
+        }
+    
+        private void loadJSONToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myFile = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "JSON files (*.json)|*.json";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myFile = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myFile)
+                        {
+                            richTextBoxFile.Text = File.ReadAllText(openFileDialog.FileName);
+                            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(RingSquare));
+                            ring = (RingSquare)ser.ReadObject(myFile);
+                        }
+                        textBoxRadius.Text = Convert.ToString(ring.r);
+                        textBoxAngle.Text = Convert.ToString(ring.a);
+                        textBoxResult.Text = Convert.ToString(ring.s);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void loadXMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myFile = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "XML files (*.xml)|*.xml";
+            openFileDialog.RestoreDirectory = true;
+            //string myFilePath = openFileDialog.FileName;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myFile = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myFile)
+                        {
+                            richTextBoxFile.Text = File.ReadAllText(openFileDialog.FileName);
+                            XmlSerializer f = new XmlSerializer(typeof(RingSquare));
+                            ring = (RingSquare)f.Deserialize(myFile);
+                        }
+                        textBoxRadius.Text = Convert.ToString(ring.r);
+                        textBoxAngle.Text = Convert.ToString(ring.a);
+                        textBoxResult.Text = Convert.ToString(ring.s);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
